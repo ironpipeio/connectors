b86102dc00878aa0be90a8051412bb0979849526
Eckart Walther <eckart@Eckarts-MacBook-Air.local>
2017-10-01 12:08:08 -0700

    Updated code

---
+++ jpg/jpg.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Ironpipe JPEG connector
+
+@author: eckart
+"""
+import ironpipe
+import PIL
+import sys
+
+def main():
+    """
+    """
+    try:
+        # read file meta data and file
+        info = ironpipe.connector_read_info(sys.stdin)
+        image = PIL.open(sys.stdin)
+    except:
+        return ironpipe.connector_error('bad image file')
+
+    # update file metadata
+    info['content-type'] = 'image/jpeg'
+    info['width'] = image.width
+    info['height'] = image.height
+
+    try:
+        # write meta data into stream]
+        ironpipe.connector_write_info(sys.stdout, info)
+        # push image back into stream
+        image.save(sys.stdout, 'JPEG')
+    except:
+        return ironpipe.connector_error('write error')
+
+    return 0
+
+if __name__ == '__main__':
+    # Remove program name from command line arguments
+    sys.exit(main())
